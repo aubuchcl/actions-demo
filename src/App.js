43a8da453de97cf12f49e9170705bd643f5b9754
Chris Aubuchon <aubuchcl@gmail.com>
2022-10-03 08:34:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hart Demo</p>
+        <p>Kiran Demo</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React HERE IS A CHNAGE
         </a>
