d781e58bbe3a8239863da71e4c60b3e7fca1a0b8
Chris Aubuchon <aubuchcl@gmail.com>
2023-11-17 10:24:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>7-11 day @ 7-11</p>
+        <p>Demo Website</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          React is pretty cool if you use redux toolkit
+          Look at this great page!
         </a>
       </header>
     </div>
