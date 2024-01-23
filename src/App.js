767902c943e247e26ec04292c2e51faff6e89c31
Chris Aubuchon <aubuchcl@gmail.com>
2024-01-23 12:21:55 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Rendering Deployment Version 1.4.1-latest</p>
+        <p>FYC DEMO</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Look at this great page!
         </a>
